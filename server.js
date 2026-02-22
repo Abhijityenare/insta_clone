620fbdf4d35aae839d8d19a932a6c3b589bde7e3
Abhijityenare <abhijityenare955@gmail.com>
2026-02-22 10:56:26 +0530

    add user registration and login API

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const connectToDB =  require('./src/config/database')
+const app = require('./src/app')
+
+
+connectToDB()
+app.listen(3000,()=>{
+    console.log('server running on port 3000');
+})
